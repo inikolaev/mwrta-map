5f7cb4c5eeffe9e2364f1f496383a47729c564e3
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2016-07-23 02:37:31 +0300

    Fixed port number

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 
+var port = process.env.PORT || 8080;
+
 var server = express();
 server.disable('x-powered-by');
 server.use('/mwrta', bodyParser.json({type: '*/*'}), require("./mwrta"));
 
-server.listen(80);
+server.listen(port);
