541dfde867800f7a4b9ad5121be8c4a52f7f1e46
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2016-07-23 02:50:27 +0300

    Still trying to make it work on Heroku

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 
-var port = process.env.PORT || 8080;
-
 var server = express();
+server.set('port', (process.env.PORT || 5000));
 server.disable('x-powered-by');
 server.use('/mwrta', bodyParser.json({type: '*/*'}), require("./mwrta"));
 
-server.listen(port);
+server.listen(server.get('port'));
