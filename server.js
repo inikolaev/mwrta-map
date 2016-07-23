4ec6719ee6b3ed188f217fde991fe6714c61ecd7
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2016-07-23 03:06:45 +0300

    Log a message when server starts

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ server.set('port', (process.env.PORT || 5000));
 server.disable('x-powered-by');
 server.use('/mwrta', bodyParser.json({type: '*/*'}), require("./mwrta"));
 
-server.listen(server.get('port'));
+server.listen(server.get('port'), function() {
+  console.log('mwrta-map is running on port', server.get('port'));
+});
