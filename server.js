3ada4b4a41ef010c24bc5a3ce266f1ceb4c715d9
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2016-07-24 23:54:56 +0300

    Redirect to HTTPS

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 
+function ensureSecure(req, res, next) {
+  if (req.secure) {
+    return next();
+  }
+
+  res.redirect('https://' + req.hostname + ':' + req.port + '/' + req.url);
+};
+
 var server = express();
 server.set('port', (process.env.PORT || 5000));
 server.disable('x-powered-by');
 server.engine('.html', require('ejs').renderFile);
+server.all('*', ensureSecure);
 server.use('/mwrta', bodyParser.json({type: '*/*'}), require("./mwrta"));
 server.use('/', express.static('.'))
 
